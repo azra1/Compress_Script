b004d4b8d1da0182c4b515a2bd0effcd509fa684
azra1 <azra191996tabassum@gmail.com>
2018-07-18 10:33:41 +0530

    SQL BACKUP WITH DEFINED PATHS

--- sqlbackup.sh
+++ sqlbackup.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -e
-
-location=~/`date +%Y%m%d_%H%M%S`.db
-now="$(date)"
-echo "Backup starting at $now....."
-mysqldump -u root --password=<your password> database_name > $location
-echo "Backup sucessfully ended at $now!!"
-gzip $location
+#!/bin/bash
+date=`date +'%d%m%Y'`
+date1=`date -d "-3 days" +"%d%m%Y"`
+echo $date1
+echo $date
+mysqldump -u'root' -p'<your password>' --routines --all-databases > /DB/Full_database_backup_$date.sql
+gzip -r /DB/Full_database_backup_$date.sql
+rm -rf /DB/Full_database_backup_$date1.sql.gz
